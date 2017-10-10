e86feabcade804147dd8486371603ffa2ee319ae
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-10 13:44:51 +0200

    Enqueue styles & js for local theme

--- wp-content/themes/karnan/library/Theme/Enqueue.php
+++ wp-content/themes/karnan/library/Theme/Enqueue.php
@@ -17,9 +17,6 @@ class Enqueue
      */
     public function style()
     {
-        wp_register_style('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/css/hbg-prime.min.css', '', '1.0.0');
-        wp_enqueue_style('hbg-prime');
-
         wp_enqueue_style('karnan-css', get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css', '', filemtime(get_stylesheet_directory() . '/assets/dist/css/app.min.css'));
     }
 
@@ -29,9 +26,6 @@ class Enqueue
      */
     public function script()
     {
-        wp_register_script('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/js/hbg-prime.min.js', '', '1.0.0', true);
-        wp_enqueue_script('hbg-prime');
-
         wp_enqueue_script('karnan-js', get_stylesheet_directory_uri(). '/assets/dist/js/app.min.js', '', filemtime(get_stylesheet_directory() . '/assets/dist/js/app.min.js'), true);
     }
 }
