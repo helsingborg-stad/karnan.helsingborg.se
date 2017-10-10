0e5b636a54d0266e80ad2e87aec292dae6a582a8
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-10 14:38:56 +0200

    Basic field configurations

--- wp-content/themes/karnan/functions.php
+++ wp-content/themes/karnan/functions.php
@@ -8,9 +8,22 @@ if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
 }
 
 require_once KARNAN_PATH . 'library/Vendor/Psr4ClassLoader.php';
-$loader = new KARNAN\Vendor\Psr4ClassLoader();
+$loader = new karnan\Vendor\Psr4ClassLoader();
 $loader->addPrefix('karnan', KARNAN_PATH . 'library');
 $loader->addPrefix('karnan', KARNAN_PATH . 'source/php/');
 $loader->register();
+
+new karnan\App();
+
+add_action('init', function () {
+    $acfExportManager = new \AcfExportManager\AcfExportManager();
+    $acfExportManager->setTextdomain('karnan');
+    $acfExportManager->setExportFolder(KARNAN_PATH . 'library/AcfFields');
+    $acfExportManager->autoExport(array(
+        'theme-settings'    => 'group_59dcbee046030',
+        'front-page'    => 'group_59dcbf10ae25f',
+    ));
+    $acfExportManager->import();
+});
 
 new karnan\App();
---
+++ wp-content/themes/karnan/library/AcfFields/php/theme-settings.php
@@ -0,0 +1,26 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_59dcbee046030',
+    'title' => __('Theme settings', 'karnan'),
+    'fields' => false,
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'post',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
