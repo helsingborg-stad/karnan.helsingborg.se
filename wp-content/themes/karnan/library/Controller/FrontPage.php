933530cb3ec377880e4d721d5c7a7c7a9736442e
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-25 15:57:41 +0200

    Renamed sections dir

--- wp-content/themes/karnan/library/Controller/FrontPage.php
+++ wp-content/themes/karnan/library/Controller/FrontPage.php
@@ -13,7 +13,7 @@ class FrontPage extends \Municipio\Controller\BaseController
         //Get background images url
         if (is_array($this->data['sections']) && !empty($this->data['sections'])) {
             foreach ($this->data['sections'] as $key => &$section) {
-                $section['background'] = get_stylesheet_directory_uri() . '/assets/image/background/' . ($key+1) . '.png';
+                $section['background'] = get_stylesheet_directory_uri() . '/assets/image/sections/' . ($key+1) . '.png';
             }
         }
 
