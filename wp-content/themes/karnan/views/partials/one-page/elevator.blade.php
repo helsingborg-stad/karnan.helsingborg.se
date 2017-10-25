d21db5e6af8a7d7c29fe67feac749c4d4df3202f
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-25 16:50:20 +0200

    Adjusted vertical text on sections.

--- wp-content/themes/karnan/views/partials/one-page/elevator.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/elevator.blade.php
@@ -1,7 +1,7 @@
 @if (is_array($sections) && !empty($sections))
     <ul id="one-page-elevator">
     @foreach ($sections as $elevatorKey => $elevatorItem)
-        <li data-tooltip="{{ $elevatorItem['height_indicator'] }} - {{ $elevatorItem['section_title'] }}" data-tooltip-right class="elevator-item @if($elevatorKey == 0) is-first @endif @if($elevatorKey == count($sections)-1) is-last @endif ">
+        <li data-tooltip="{{ $elevatorItem['height_indicator'] }}<?php _e("m", 'karnan'); ?> - {{ $elevatorItem['section_title'] }}" data-tooltip-right class="elevator-item @if($elevatorKey == 0) is-first @endif @if($elevatorKey == count($sections)-1) is-last @endif ">
             <a href="#{{ sanitize_title($elevatorItem['section_title']) }}">
                 <span class="blipper blipper-theme-1"></span>
                 <span class="blipper-disabled"></span>
