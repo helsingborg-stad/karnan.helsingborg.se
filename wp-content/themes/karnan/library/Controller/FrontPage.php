c699c84077320ba10030eb67cb655256a0519667
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-26 13:36:28 +0200

    Get a url to the virtual sections page

--- wp-content/themes/karnan/library/Controller/FrontPage.php
+++ wp-content/themes/karnan/library/Controller/FrontPage.php
@@ -4,8 +4,13 @@ namespace karnan\Controller;
 
 class FrontPage extends \Municipio\Controller\BaseController
 {
+    private $db = null;
+
     public function init()
     {
+
+        //Define global as local value
+        $this->globalToLocal("wpdb", "db");
 
         //Get entered details
         $this->data['sections'] = is_array(get_field('karnan_sections', 'option')) ? get_field('karnan_sections', 'option') : array();
@@ -17,5 +22,35 @@ class FrontPage extends \Municipio\Controller\BaseController
             }
         }
 
+        //Virtual guide link
+        if ($guideLink = $this->getVirtualGuideLink()) {
+            $this->data['virtualGuidePage'] = $guideLink;
+        } else {
+            $this->data['virtualGuidePage'] = null;
+        }
+    }
+
+    public function getVirtualGuideLink()
+    {
+        $page_id =  $this->db->get_var(
+                        "SELECT post_id FROM ". $this->db->postmeta  ." WHERE meta_key = '_wp_page_template' AND meta_value = 'virtual-section.blade.php' LIMIT 1"
+                    );
+
+        if (is_numeric($page_id) && $permalink = get_permalink($page_id)) {
+            return $permalink;
+        }
+
+        return false;
+    }
+
+    public function globalToLocal($global, $local = null)
+    {
+        global $$global;
+
+        if (is_null($local)) {
+            $this->$global = $$global;
+        } else {
+            $this->$local = $$global;
+        }
     }
 }
