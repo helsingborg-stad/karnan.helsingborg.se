c1f8a6d37c285ef2389cee86b489bb99a7e5069b
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-27 15:30:12 +0200

    Updated color choises.

--- wp-content/themes/karnan/views/partials/one-page/scroll-down.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/scroll-down.blade.php
@@ -1,4 +1,4 @@
-<a class="scroll-action scroll-down btn btn-primary disabled hidden-xs" href="#down">
+<a class="scroll-action scroll-down btn btn-theme-fourth disabled hidden-xs" href="#down">
     <span aria-hidden="true" class="sr-only"><?php _e("Go down", 'karnan'); ?></span>
     <i class="pricon pricon-chevron-down"></i>
 </a>
