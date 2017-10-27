a5bcbb4ae138d1da34f3c24e375f3e63011b511b
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-27 15:19:34 +0200

    Hide scroll actions on really small devices.

--- wp-content/themes/karnan/views/partials/one-page/scroll-up.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/scroll-up.blade.php
@@ -1,4 +1,4 @@
-<a class="scroll-action scroll-up btn btn-primary disabled" href="#up">
+<a class="scroll-action scroll-up btn btn-primary disabled hidden-xs" href="#up">
     <span aria-hidden="true" class="sr-only"><?php _e("Go up", 'karnan'); ?></span>
     <i class="pricon pricon-chevron-up"></i>
 </a>
