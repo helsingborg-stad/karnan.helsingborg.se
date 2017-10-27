78b15392581a7c75e2ac34512ad9564da91e9133
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-10-27 10:39:30 +0200

    Karnan section part image

--- wp-content/themes/karnan/views/partials/virtual/image.blade.php
+++ wp-content/themes/karnan/views/partials/virtual/image.blade.php
@@ -1,5 +1,8 @@
 <div class="grid-lg-4 grid-md-12">
     @if(!empty($section['section_image']['url']))
-        <img src="{{ $section['section_image']['url'] }}" class="image virtual-section-image" alt="{{ $section['section_title'] }}">
+        <img src="{{ $section['section_image']['url'] }}" class="image virtual-section-image hidden-xs hidden-sm hidden-md" alt="{{ $section['section_title'] }}">
+    @endif
+    @if(!empty($section['section_image_part']['url']))
+        <img src="{{ $section['section_image_part']['url'] }}" class="image virtual-section-image hidden-lg" alt="{{ $section['section_title'] }}">
     @endif
 </div>
