6d2eeb5d3274bd503ae255b957a1e0d46e0acd43
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-10-27 10:41:41 +0200

    Takes admin bar in consideration

--- wp-content/themes/karnan/views/front-page.blade.php
+++ wp-content/themes/karnan/views/front-page.blade.php
@@ -31,6 +31,20 @@
     </div>
 </div>
 
+<!--
+<div class="pixel-wrapper">
+<div class="pixel" data-stellar-ratio="1.1"></div>
+<div class="pixel" data-stellar-ratio="1.2"></div>
+<div class="pixel" data-stellar-ratio="1.3"></div>
+<div class="pixel" data-stellar-ratio="1.4"></div>
+<div class="pixel" data-stellar-ratio="1.5"></div>
+<div class="pixel" data-stellar-ratio="1.6"></div>
+<div class="pixel" data-stellar-ratio="1.7"></div>
+<div class="pixel" data-stellar-ratio="1.8"></div>
+<div class="pixel" data-stellar-ratio="1.9"></div>
+<div class="pixel" data-stellar-ratio="2.0"></div>
+</div> -->
+
 @include ('partials/one-page/scroll-down')
 
 @stop
