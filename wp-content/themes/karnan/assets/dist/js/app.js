df319499d363a5de71f7e7b7f6cfde3cbac1684c
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-11-07 10:39:04 +0100

    Gulp + add package-lock.json

--- wp-content/themes/karnan/assets/dist/js/app.js
+++ wp-content/themes/karnan/assets/dist/js/app.js
@@ -1796,6 +1796,81 @@ Karnan.OnePage.ScrollSnapping = (function ($) {
         if ($(".virtual-section").length) {
             this.VirtualSections();
         }
+
+        //Total section count (starting form 0)
+        this.sectionCount = $(".onepage-section").length - 1;
+
+        this.startState();
+    }
+
+    ScrollSnapping.prototype.startState = function () {
+        //Set last section as current if no hash is defined
+        if(window.location.hash == "#start") {
+            $.scrollify.instantMove(this.sectionCount);
+        }
+
+        //Add active class
+        $.scrollify.current().addClass('active-section');
+
+        //Add wrapper class
+        $('#one-page-content').addClass('active-section-' + $.scrollify.currentIndex());
+
+        //Add next class
+        if ($.scrollify.currentIndex() - 1 > 0) {
+            $.scrollify.current().prev().addClass('next-section');
+        }
+
+        //Add prev class
+        if ($.scrollify.currentIndex() + 1 <= this.sectionCount) {
+            $.scrollify.current().next().addClass('prev-section');
+        }
+    }
+
+    ScrollSnapping.prototype.updateWrapper = function (index, sections) {
+        //Remove old wrapper class
+        $('#one-page-content').removeClass (function (index, className) {
+            return (className.match (/(^|\s)active-section-\S+/g) || []).join(' ');
+        });
+
+        //Add active
+        $('#one-page-content').addClass('active-section-' + index);
+    }
+
+
+    ScrollSnapping.prototype.updateActive = function (index, sections) {
+        //Remove active class
+        if (sections[(index + 1)] && sections[(index + 1)].hasClass('active-section')) {
+            sections[(index + 1)].removeClass('active-section');
+        } else if (sections[(index - 1)] && sections[(index - 1)].hasClass('active-section')) {
+            sections[(index - 1)].removeClass('active-section');
+        }
+
+        //Set new active class
+        sections[index].addClass('active-section');
+    }
+
+    ScrollSnapping.prototype.updateNext = function (index, sections) {
+        //Remove old next
+        if ($('.next-section')) {
+            $('.next-section').removeClass('next-section');
+        }
+
+        //Set new next
+        if (sections[(index - 1)]) {
+            sections[(index - 1)].addClass('next-section');
+        }
+    }
+
+    ScrollSnapping.prototype.updatePrev = function (index, sections) {
+        //Remove old prev
+        if ($('.prev-section')) {
+            $('.prev-section').removeClass('prev-section');
+        }
+
+        //Set new next
+        if (sections[(index + 1)]) {
+            sections[(index + 1)].addClass('prev-section');
+        }
     }
 
     ScrollSnapping.prototype.StartSections = function () {
@@ -1803,11 +1878,17 @@ Karnan.OnePage.ScrollSnapping = (function ($) {
         var scrollSpeed = 800;
 
         $.scrollify({
+            setHeights : true,
             section : ".onepage-section",
             sectionName : "section-name",
             scrollSpeed: scrollSpeed,
             easing: "easeOutExpo",
             before: function(index, sections) {
+                this.updateActive(index, sections);
+                this.updateNext(index, sections);
+                this.updatePrev(index, sections);
+                this.updateWrapper(index, sections);
+
                 $(document).trigger('scrollifyStart', [index, sections, scrollSpeed, 'onepage']);
             }.bind(this),
             after: function(index, sections) {
