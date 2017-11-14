1d5ee6ef4e111244a6b4b337c74fc30dd14a7acd
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-11-14 15:42:46 +0100

    Modal: Disable body scroll fix

--- wp-content/themes/karnan/views/partials/one-page/modal.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/modal.blade.php
@@ -1,7 +1,8 @@
-<div id="modal-target-{{ sanitize_title($section['section_title']) }}" class="modal modal-backdrop-white modal-large modal-karnan" tabindex="-1" role="dialog" aria-hidden="true">
+<div id="modal-target-{{ sanitize_title($section['section_title']) }}" data-section-name="{{ sanitize_title($section['section_title']) }}" class="modal modal-backdrop-white modal-large modal-karnan" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-content material-shadow-lg">
         <div class="modal-header">
-            <a class="btn btn-close close" href="#close"></a>
+            <h2 class="modal-title">{{ $section['section_title'] }}</h2>
+            <a class="btn btn-close close" href="#{{ sanitize_title($section['section_title']) }}"></a>
         </div>
         <div class="modal-body container">
             <div class="grid">
@@ -14,7 +15,7 @@
                 </div>
                 <div class="grid-md-7">
                     <div class="content">
-                        <h2 class="modal-title">{{ $section['section_title'] }}</h2>
+
                         @if(!empty($section['section_image']['url']))
                         <img class="hidden-md hidden-lg" src="{{ $section['section_image']['url'] }}" alt="{{ $section['section_title'] }}">
                         @endif
@@ -32,14 +33,18 @@
                                 @endforeach
                             </ul>
                         </div>
+
+                        <div class="virtual-360">
+                               <iframe src="{{ $section['google_maps'] }}" width="100%" height="100vh" frameborder="0" style="border:0" allowfullscreen></iframe>
+                        </div>
                     @endif
                 </div>
             </div>
         </div>
 
         <div class="modal-footer">
-            <a href="#close" class="btn btn-default close"><?php _e('Close', 'karnan'); ?></a>
+            <a href="#{{ sanitize_title($section['section_title']) }}" class="btn btn-default btn-block close"><?php _e('Close', 'karnan'); ?></a>
         </div>
     </div><!-- /.modal-content -->
-    <a href="#close" class="backdrop"></a>
+    <a href="#" class="backdrop"></a>
 </div><!-- /.modal -->
