1f34f6713e3613279d291edb0affd09f37d32003
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-11-14 15:22:10 +0100

    Preloader shadow & translation

--- wp-content/themes/karnan/views/partials/one-page/preloader.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/preloader.blade.php
@@ -1,8 +1,8 @@
 <div id="preloader">
+    <div id="preloader-blipper">
+        <div class="blipper blipper-theme-3"></div>
+    </div>
     <div id="preloader-content">
-        <div id="preloader-blipper">
-            <div class="blipper blipper-theme-3"></div>
-        </div>
         <span id="preloader-icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
                 <path fill="#FFF" d="M331 0v30h-30V0h-90v30h-30V0H91v512h330V0h-90zm-60 482h-30V382h30v100zm120 0h-90V352h-90v130h-90V120h30v30h30v-30h30v30h30v-30h30v30h30v-30h30v30h30v-30h30v362zm0-392H121V30h30v30h90V30h30v30h90V30h30v60z"/>
