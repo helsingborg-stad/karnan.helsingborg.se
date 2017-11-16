d84383c20c5499a23e9f262ec662920aa1019f75
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-11-16 16:50:39 +0100

    Removed virtual sections template, controller and view

--- wp-content/themes/karnan/library/Controller/VirtualSection.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Karnan\Controller;
-
-class VirtualSection extends \Karnan\Controller\FrontPage
-{
-    //Placeholder controller. This just extends front page controller to inherit data.
-}
