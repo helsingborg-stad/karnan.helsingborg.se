ee98fe5c18e00bc69cb7be73df2bc84c287955a6
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-11-16 11:44:12 +0100

    Translations

--- wp-content/themes/karnan/views/partials/one-page/scroll-up.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/scroll-up.blade.php
@@ -1,6 +1,6 @@
 <div class="scroll-action-wrapper up">
 <a class="scroll-action scroll-up disabled btn btn-plain" href="#up">
-    <span class="hidden-xs standard-text"><?php _e("Gå upp en våning", 'karnan'); ?></span>
-    <span class="hidden-xs video-text"><?php _e("Gå till live video", 'karnan'); ?></span>
+    <span class="hidden-xs standard-text"><?php _e("Go down a floor", 'karnan'); ?></span>
+    <span class="hidden-xs video-text"><?php _e("Go to live video", 'karnan'); ?></span>
 </a>
 </div>
