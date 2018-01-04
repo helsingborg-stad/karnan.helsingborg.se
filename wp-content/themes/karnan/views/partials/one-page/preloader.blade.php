f27ee561b33ea1af603a1db35bc4d0ad56d70c1a
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-01-04 12:57:17 +0100

    add new preloader icon & change background color

--- wp-content/themes/karnan/views/partials/one-page/preloader.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/preloader.blade.php
@@ -4,10 +4,7 @@
     </div>
     <div id="preloader-content">
         <span id="preloader-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
-                <path fill="#FFF" d="M331 0v30h-30V0h-90v30h-30V0H91v512h330V0h-90zm-60 482h-30V382h30v100zm120 0h-90V352h-90v130h-90V120h30v30h30v-30h30v30h30v-30h30v30h30v-30h30v30h30v-30h30v362zm0-392H121V30h30v30h90V30h30v30h90V30h30v60z"/>
-                <path fill="#FFF" d="M211 202v90h90v-90h-90zm60 60h-30v-30h30v30z"/>
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 115.67 164.93"><defs><style>.tower-path{fill:#4db4e7;fill-rule:evenodd;}</style></defs><title>tower</title><path class="tower-path" d="M91.52,42.15V60.56H69.91V42.15H45.76V60.56H24.14V42.15H0V68.82A12.09,12.09,0,0,0,9.85,80.66v84.27H105.1V80.74a12.08,12.08,0,0,0,10.57-11.92V42.15ZM77.16,146H61.82V130.34a7.67,7.67,0,0,1,15.34,0Zm0-40.14H61.82V90.2a7.67,7.67,0,0,1,15.34,0Z"/><polygon class="tower-path" points="54.69 21.11 89.11 11.38 45.76 0 45.76 33.7 54.69 33.7 54.69 21.11"/></svg>
         </span>
         <span id="preloader-text"><?php _e("Loading…", 'karnan'); ?></span>
     </div>
