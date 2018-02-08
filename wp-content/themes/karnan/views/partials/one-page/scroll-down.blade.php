9692a10981f2099a35be08055d768b27def71f32
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-02-08 13:48:49 +0100

    add background to scroll action button

--- wp-content/themes/karnan/views/partials/one-page/scroll-down.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/scroll-down.blade.php
@@ -1,5 +1,5 @@
 <div class="scroll-action-wrapper down">
-    <a class="scroll-action scroll-down disabled btn btn-plain" href="#down">
+    <a class="scroll-action scroll-down disabled btn btn-theme-second" href="#down">
         <span class="hidden-xs"><?php _e("Go down a floor", 'karnan'); ?></span>
     </a>
 </div>
--- wp-content/themes/karnan/views/partials/one-page/scroll-up.blade.php
+++ wp-content/themes/karnan/views/partials/one-page/scroll-up.blade.php
@@ -1,5 +1,5 @@
 <div class="scroll-action-wrapper up">
-<a class="scroll-action scroll-up disabled btn btn-plain" href="#up">
+<a class="scroll-action scroll-up disabled btn btn-theme-second" href="#up">
     <span class="hidden-xs standard-text"><?php _e("Go up a floor", 'karnan'); ?></span>
     <span class="hidden-xs video-text"><?php _e("Go to live video", 'karnan'); ?></span>
 </a>
